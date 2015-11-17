094c641b62f9793c820048e8133856dff14f8d92
Antonis Christofides <anthony@itia.ntua.gr>
2015-11-17 11:36:09 +0200

    Version 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-selenium-clean",
-    version="0.2.0",
+    version="0.2.1",
     license="GPL3",
     description="Write clean Selenium tests in Django",
     author="Antonis Christofides",
