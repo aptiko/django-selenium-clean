aptiko/django-selenium-clean

commit 2c03c0ea115fc7045ff2fdcb879d928215a79d89

Fix a PEP8 issue (imports at the top)

django_selenium_clean/__init__.py
@@ -24,7 +24,19 @@ class SeleniumWrapper(object):
         return cls._instance
 
     def __init__(self):
-        self.driver = False
+        self.__dict__['driver'] = False
+
+    def ensure_init(self):
+        """Perform deferred initialization
+        We can't run the following code in __init__() because then it will
+        run at import time (because the SeleniumWrapper object is immediately
+        instantiated in the selenium global variable). We don't want to run
+        it at import time because Django might not yet be fully setup (meaning
+        settings might not yet be available.
+        """
+        if self.__dict__['driver']:
+            # We are already initialized, nothing to do
+            return
         SELENIUM_WEBDRIVERS = getattr(settings, 'SELENIUM_WEBDRIVERS', {})
         if not SELENIUM_WEBDRIVERS:
             return
@@ -33,18 +45,21 @@ class SeleniumWrapper(object):
         callable = driver['callable']
         args = driver['args']
         kwargs = driver['kwargs']
-        self.driver = callable(*args, **kwargs)
+        self.__dict__['driver'] = callable(*args, **kwargs)
 
     def __getattr__(self, name):
+        self.ensure_init()
         return getattr(self.driver, name)
 
     def __setattr__(self, name, value):
+        self.ensure_init()
         if name == 'driver':
             self.__dict__[name] = value
         else:
             setattr(self.driver, name, value)
 
     def __nonzero__(self):
+        self.ensure_init()
         return bool(self.driver)
 
     def login(self, **credentials):
@@ -57,6 +72,8 @@ class SeleniumWrapper(object):
 
         The code is based on django.test.client.Client.login.
         """
+        self.ensure_init()
+
         # Visit the home page to ensure the cookie gets the proper domain
         self.get(self.live_server_url)
 
@@ -94,6 +111,7 @@ class SeleniumWrapper(object):
 
         Causes the authenticated user to be logged out.
         """
+        self.ensure_init()
         session = import_module(settings.SESSION_ENGINE).SessionStore()
         session_cookie = self.get_cookie(settings.SESSION_COOKIE_NAME)
         if session_cookie:
@@ -101,6 +119,7 @@ class SeleniumWrapper(object):
             self.delete_cookie(settings.SESSION_COOKIE_NAME)
 
     def wait_until_n_windows(self, n, timeout=2):
+        self.ensure_init()
         for i in range(timeout * 10):
             if len(self.window_handles) == n:
                 return

tests/tests.py
@@ -2,15 +2,7 @@ import os
 
 import django
 from django.core import management
-
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
-django.setup()
-management.call_command('migrate')
-
-
 from django.contrib.auth.hashers import make_password
-from django.contrib.auth.models import User
 
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
@@ -50,6 +42,8 @@ class DjangoSeleniumCleanTestCase(SeleniumTestCase):
         self.assertFalse(self.heading_world.is_displayed())
 
     def test_login(self):
+        from django.contrib.auth.models import User
+
         User.objects.create(username='alice',
                             password=make_password('topsecret'),
                             is_active=True)
@@ -186,3 +180,8 @@ class DjangoSeleniumCleanTestCase(SeleniumTestCase):
             self.message.wait_until_not_contains('world', timeout=1)
         self.assertTrue('world' in self.message.text)
         self.assertFalse('earth' in self.message.text)
+
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+django.setup()
+management.call_command('migrate')

commit e379c17d11f53a8055b5b302130aebf6e87e4925

Fix inability to run on Python 2
This bug was introduced in 2c03c0ea115. At least it affected
django-selenium-clean's unit tests. I don't know why it was working in
Python 3.

django_selenium_clean/__init__.py
@@ -58,8 +58,7 @@ class SeleniumWrapper(object):
             setattr(self.driver, name, value)
 
     def __nonzero__(self):
-        self.ensure_init()
-        return bool(self.driver)
+        return bool(self.__dict__['driver'])
 
     def login(self, **credentials):
         """