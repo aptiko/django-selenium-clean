52078b3232214b283d2e5cc5bc3f6c8960ebb74c
Antonis Christofides <antonis@antonischristofides.com>
2019-08-30 15:17:02 +0300

    Black, isort, flake8

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
-
+from setuptools import find_packages, setup
 
 setup(
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Software Development :: Testing',
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Testing",
     ],
     name="django-selenium-clean",
     version="0.3.2",
@@ -25,9 +24,6 @@ setup(
     author_email="antonis@djangodeployment.com",
     url="https://github.com/aptiko/django-selenium-clean",
     packages=find_packages(),
-    install_requires=[
-        'django>=1.11,<3',
-        'selenium>=2.40,<4',
-    ],
+    install_requires=["django>=1.11,<3", "selenium>=2.40,<4"],
     test_suite="tests.tests",
 )
--- tests/tests.py
+++ tests/tests.py
@@ -3,7 +3,7 @@ from unittest import SkipTest
 
 import django
 from django.core import management
-from django.test import override_settings, TestCase
+from django.test import TestCase, override_settings
 
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
@@ -13,15 +13,15 @@ from django_selenium_clean import PageElement, SeleniumTestCase
 
 class DjangoSeleniumCleanTestCase(SeleniumTestCase):
 
-    heading_earth = PageElement(By.ID, 'earth')
-    heading_world = PageElement(By.ID, 'world')
-    user_info = PageElement(By.ID, 'user')
-    button_toggle_heading = PageElement(By.ID, 'toggle-heading')
-    button_open_window = PageElement(By.ID, 'open-window')
-    button_toggle_element = PageElement(By.ID, 'toggle-element')
-    button_toggle_message = PageElement(By.ID, 'toggle-message')
-    togglable = PageElement(By.ID, 'togglable')
-    message = PageElement(By.ID, 'message')
+    heading_earth = PageElement(By.ID, "earth")
+    heading_world = PageElement(By.ID, "world")
+    user_info = PageElement(By.ID, "user")
+    button_toggle_heading = PageElement(By.ID, "toggle-heading")
+    button_open_window = PageElement(By.ID, "open-window")
+    button_toggle_element = PageElement(By.ID, "toggle-element")
+    button_toggle_message = PageElement(By.ID, "toggle-message")
+    togglable = PageElement(By.ID, "togglable")
+    message = PageElement(By.ID, "message")
 
     def test_toggle(self):
         self.selenium.get(self.live_server_url)
@@ -47,26 +47,26 @@ class DjangoSeleniumCleanTestCase(SeleniumTestCase):
         from django.contrib.auth.models import User
 
         cap = self.selenium.capabilities
-        if cap["browserName"] == 'phantomjs' and cap["version"] == '2.1.1':
+        if cap["browserName"] == "phantomjs" and cap["version"] == "2.1.1":
             raise SkipTest("https://github.com/ariya/phantomjs/issues/14228")
 
-        User.objects.create(username='alice',
-                            password=make_password('topsecret'),
-                            is_active=True)
+        User.objects.create(
+            username="alice", password=make_password("topsecret"), is_active=True
+        )
 
         # Verify we aren't logged on
         self.selenium.get(self.live_server_url)
         self.user_info.wait_until_is_displayed()
-        self.assertEqual(self.user_info.text, 'No user is logged on.')
+        self.assertEqual(self.user_info.text, "No user is logged on.")
 
         # Log on
-        r = self.selenium.login(username='alice', password='topsecret')
+        r = self.selenium.login(username="alice", password="topsecret")
         self.assertTrue(r)
 
         # Verify we are logged on
         self.selenium.get(self.live_server_url)
         self.user_info.wait_until_is_displayed()
-        self.assertEqual(self.user_info.text, 'The logged on user is alice.')
+        self.assertEqual(self.user_info.text, "The logged on user is alice.")
 
         # Log out
         self.selenium.logout()
@@ -74,7 +74,7 @@ class DjangoSeleniumCleanTestCase(SeleniumTestCase):
         # Verify we are logged out
         self.selenium.get(self.live_server_url)
         self.user_info.wait_until_is_displayed()
-        self.assertEqual(self.user_info.text, 'No user is logged on.')
+        self.assertEqual(self.user_info.text, "No user is logged on.")
 
     def test_wait_until_n_windows(self):
         self.selenium.get(self.live_server_url)
@@ -165,37 +165,37 @@ class DjangoSeleniumCleanTestCase(SeleniumTestCase):
 
         # Message does not contain 'world'. Check that the various waits
         # and asserts are OK.
-        self.message.wait_until_contains('earth')
-        self.message.wait_until_not_contains('world')
+        self.message.wait_until_contains("earth")
+        self.message.wait_until_not_contains("world")
         with self.assertRaises(TimeoutException):
-            self.message.wait_until_contains('world', timeout=1)
+            self.message.wait_until_contains("world", timeout=1)
         with self.assertRaises(TimeoutException):
-            self.message.wait_until_not_contains('earth', timeout=1)
-        self.assertTrue('earth' in self.message.text)
-        self.assertFalse('world' in self.message.text)
+            self.message.wait_until_not_contains("earth", timeout=1)
+        self.assertTrue("earth" in self.message.text)
+        self.assertFalse("world" in self.message.text)
 
         # Toggle...
         self.button_toggle_message.click()
 
         # and check things are opposite
-        self.message.wait_until_contains('world')
-        self.message.wait_until_not_contains('earth')
+        self.message.wait_until_contains("world")
+        self.message.wait_until_not_contains("earth")
         with self.assertRaises(TimeoutException):
-            self.message.wait_until_contains('earth', timeout=1)
+            self.message.wait_until_contains("earth", timeout=1)
         with self.assertRaises(TimeoutException):
-            self.message.wait_until_not_contains('world', timeout=1)
-        self.assertTrue('world' in self.message.text)
-        self.assertFalse('earth' in self.message.text)
+            self.message.wait_until_not_contains("world", timeout=1)
+        self.assertTrue("world" in self.message.text)
+        self.assertFalse("earth" in self.message.text)
 
 
 @override_settings(SELENIUM_WEBDRIVERS=False)
 class DjangoSeleniumCleanSkipTestCase(TestCase):
-
     def test_skip_test(self):
         # Create and instantiate a dummy SeleniumTestCase
         class SkippedTestCase(SeleniumTestCase):
             def runTest(self):
                 pass
+
         instance = SkippedTestCase()
 
         # OK, now let's "execute" the dummy test case. Any attempt to access
@@ -209,6 +209,6 @@ class DjangoSeleniumCleanSkipTestCase(TestCase):
         instance()
 
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+os.environ["DJANGO_SETTINGS_MODULE"] = "tests.settings"
 django.setup()
-management.call_command('migrate')
+management.call_command("migrate")
