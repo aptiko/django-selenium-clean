eb9364bd00d2a7dff48df9e715aec16f6db6cd59
Antonis Christofides <antonis@antonischristofides.com>
2024-04-22 15:59:47 +0300

    Version 1.0.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
         "Topic :: Software Development :: Testing",
     ],
     name="django-selenium-clean",
-    version="1.0.0",
+    version="1.0.1",
     license="BSD 3-Clause License",
     description="Write clean Selenium tests in Django",
     author="Antonis Christofides",
